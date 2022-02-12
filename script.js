c1c3280cab9a31cbb3766bb124ad4d82744ea319
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-02-12 13:16:44 +0330

    selectApi

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 const button = document.querySelector("form-control");
 const form = document.querySelector("d-flex");
 const glass = document.getElementById("glass");
+const select = document.getElementById("episod-select");
 //?  here i wanna take the form and add an event listener for search
+// todo
+// form.addEventListener("submit", async function (e) {
+//   e.preventDefault();
+//   const searchInput = e.target.value;
 
-form.addEventListener("submit", async function (e) {
-  e.preventDefault();
-  const searchInput = e.target.value;
+//   const res = await axios.get(
+//     `https://api.tvmaze.com/search/shows?q=${searchInput}`
+//   );
 
-  const res = await axios.get(
-    `https://api.tvmaze.com/search/shows?q=${searchInput}`
-  );
+//   makeImages(res.data);
+//   e.target.value = "";
+// });
 
-  makeImages(res.data);
-  e.target.value = "";
-});
-
-const makeImages = (shows) => {
-  for (let result of shows) {
-    if (result.show.image) {
-      const img = document.createElement("IMG");
-      img.src = result.show.image.medium;
-      glass.append(img);
-    }
-  }
-};
-
+// const makeImages = (shows) => {
+//   for (let result of shows) {
+//     if (result.show.image) {
+//       const img = document.createElement("IMG");
+//       img.src = result.show.image.medium;
+//       glass.append(img);
+//     }
+//   }
+// };
 const episodes = async () => {
   //  ? trying to bring the api to website and making an option tag for it
-  try {
-    const res = await axios.get("https://api.tvmaze.com/shows/5/episodes", {
-      headers: {
-        Accept: "application/json",
-      },
-    });
-    return res.data.joke;
-  } catch (error) {
-    console.log("no jokes available , sorry");
+
+  const res = await axios.get("https://api.tvmaze.com/shows/5/episodes");
+  for (let i = 0; i < res.length; i++) {
+    const ses = res[i].season;
+    console.log(ses);
   }
 };
