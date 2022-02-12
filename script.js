ea63370e19e0002f1735922bb0e8dd728a23679b
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-02-12 12:56:22 +0330

    api intro

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const button = document.querySelector("form-control");
 const form = document.querySelector("d-flex");
 const glass = document.getElementById("glass");
-//?  here i wanna take the form
+//?  here i wanna take the form and add an event listener for search
+
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchInput = e.target.value;
@@ -21,5 +22,19 @@ const makeImages = (shows) => {
       img.src = result.show.image.medium;
       glass.append(img);
     }
+  }
+};
+
+const episodes = async () => {
+  //  ? trying to bring the api to website and making an option tag for it
+  try {
+    const res = await axios.get("https://api.tvmaze.com/shows/5/episodes", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    return res.data.joke;
+  } catch (error) {
+    console.log("no jokes available , sorry");
   }
 };
