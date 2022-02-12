410fd48b83857ed9cea610b64450fd29d624414c
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-02-12 19:30:32 +0330

    select sone

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-const button = document.querySelector("form-control");
-const form = document.querySelector("d-flex");
+const button = document.querySelector(".form-control");
+const form = document.querySelector(".d-flex");
 const glass = document.getElementById("glass");
-const select = document.getElementById("episod-select");
+const select = document.querySelector(".form-select");
+
 //?  here i wanna take the form and add an event listener for search
 // todo
-// form.addEventListener("submit", async function (e) {
-//   e.preventDefault();
-//   const searchInput = e.target.value;
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
 
-//   const res = await axios.get(
-//     `https://api.tvmaze.com/search/shows?q=${searchInput}`
-//   );
+  const res = await axios.get(
+    `https://api.tvmaze.com/search/shows/5?q=${searchTerm}`
+  );
 
-//   makeImages(res.data);
-//   e.target.value = "";
-// });
+  makeImages(res.data);
+  form.elements.query.value = "";
+});
 
-// const makeImages = (shows) => {
-//   for (let result of shows) {
-//     if (result.show.image) {
-//       const img = document.createElement("IMG");
-//       img.src = result.show.image.medium;
-//       glass.append(img);
-//     }
-//   }
-// };
-const episodes = async () => {
-  //  ? trying to bring the api to website and making an option tag for it
-
-  const res = await axios.get("https://api.tvmaze.com/shows/5/episodes");
-  for (let i = 0; i < res.length; i++) {
-    const ses = res[i].season;
-    console.log(ses);
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
   }
 };
+const episodes = async () => {
+  //  ? trying to bring the api to website and making a select tag for it
+  const res = await axios.get("https://api.tvmaze.com/shows/5/episodes");
+  return res;
+};
+
+const data = episodes().then((res) => {
+  console.log(res);
+  for (let i = 0; i < res.data.length; i++) {
+    const ses = res.data[i].season;
+    const epi = res.data[i].number;
+    const option = document.createElement("option");
+    option.innerText = `S0${ses}E0${epi}`;
+    console.log(select);
+    select.append(option);
+  }
+});
