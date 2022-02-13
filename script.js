a5d1cccdf38d53a7f0c4f2cb6ea835b9481d05ca
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-02-13 18:40:23 +0330

    search intro

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ const select = document.querySelector(".form-select");
 
 //?  here i wanna take the form and add an event listener for search
 // todo
-form.addEventListener("submit", async function (e) {
-  e.preventDefault();
-  const searchTerm = form.elements.query.value;
+// form.addEventListener("submit", async function (e) {
+//   e.preventDefault();
+//   const searchTerm = form.elements.query.value;
 
-  const res = await axios.get(
-    `https://api.tvmaze.com/search/shows/5?q=${searchTerm}`
-  );
+//   const res = await axios.get(
+//     `https://api.tvmaze.com/search/shows/5?q=${searchTerm}`
+//   );
 
-  makeImages(res.data);
-  form.elements.query.value = "";
-});
+//   makeImages(res.data);
+//   form.elements.query.value = "";
+// });
 
-const makeImages = (shows) => {
-  for (let result of shows) {
-    if (result.show.image) {
-      const img = document.createElement("IMG");
-      img.src = result.show.image.medium;
-      document.body.append(img);
-    }
-  }
-};
+// const makeImages = (shows) => {
+//   for (let result of shows) {
+//     if (result.show.image) {
+//       const img = document.createElement("IMG");
+//       img.src = result.show.image.medium;
+//       document.body.append(img);
+//     }
+//   }
+// };
 const episodes = async () => {
   //  ? trying to bring the api to website and making a select tag for it
   const res = await axios.get("https://api.tvmaze.com/shows/5/episodes");
@@ -80,11 +80,42 @@ const data = episodes().then((res) => {
     });
   }
   // ? appending all the series with name , season , summary , image and link
- 
-  
+
   //   for (let i = 0; i < res.data.length; i++) {
   //     const ses = res.data[i].season;
   //     const epi = res.data[i].number;
 
   //   }
+});
+// ! from search
+// todo complete the code below, then make a refresh button to refresh search and select results , finally make a footer for site
+const data2 = episodes().then((res) => {
+  console.log(res);
+
+  const search = document.getElementById("search");
+  const results = glass;
+  let searchTerm = "";
+
+  const showList = () => {
+    results.innerHTML = "";
+    data2
+      .filter((item) => {
+        return (
+          item.country.toLowerCase().includes(searchTerm) ||
+          item.name.toLowerCase().includes(searchTerm)
+        );
+      })
+      .forEach((e) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<i>Name:</i> ${e.name}  || <i>Country:</i> ${e.country}`;
+        results.appendChild(li);
+      });
+  };
+
+  showList();
+
+  search.addEventListener("input", (event) => {
+    searchTerm = event.target.value.toLowerCase();
+    showList();
+  });
 });
